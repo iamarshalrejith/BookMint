32c66e9848c637690cd47f35298933ced8834561
Arshal Rejith <arshalrejithcodes@gmail.com>
2026-07-25 23:25:55 +0530

    feat: expose booking HTTP endpoints

---
+++ internal/booking/handler.go
@@ -0,0 +1,13 @@
+package booking
+
+import "net/http"
+
+type handler struct {
+	svc *Service
+}
+
+func NewHandler(svc *Service) *handler {
+	return &handler{}
+}
+
+func (h *handler) ListSeats(w http.ResponseWriter, r *http.Request)
